ff1882676012b310ab93a9395f5893cd4a71f1a5
SUDHEER341 <sudheer601220@gmail.com>
2023-02-06 17:29:40 +0530

    Reverse a Number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,2 +1,16 @@
-package com.logicalprograms;public class ReverseNumber {
+package com.logicalprograms;
+
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        int number = 987654, reverse = 0;
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+       int temp = 987654;
+        System.out.println("The reverse of the  " + temp + "is----  " + reverse);
+    }
 }
